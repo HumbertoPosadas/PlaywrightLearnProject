a6185f56b4e9c4b46ab9f50e4cad2235b5b234eb
Humberto POSADAS <posadas.humberto@outlook.com>
2025-10-14 10:54:53 +0200

    Architecture, Composants et Navigation

--- Tests/04-ArchitectureEtComposants.spec.ts
+++ Tests/04-ArchitectureEtComposants.spec.ts
@@ -1,18 +1,17 @@
 import { test, Browser, Page } from '@playwright/test';      //import des librairies necessaires
 
-(async () => {      //fonction asynchrone auto-executante
-    let browser: Browser;
-    let page: Page;
-
     test.describe('Feature', () => {      //debut du bloc de tests
         test('scenario 1', async({ page }) => {      //debut du test scenario 1
             await test.step('Etant donné que je suis sur la page d\'accueil', async () => {      //debut de l'etape 1
-                await page.goto('https://www.isagri.com/');      //aller a la page d'accueil
+                await page.goto('https://www.isagri.fr/');      //aller a la page d'accueil
             })
 
-            await test.step('Quand je clique sur le lien "Solutions"', async () => {      //debut de l'etape 2
-                await page.getByRole('link', { name: 'Solutions' }).click();      //cliquer sur le lien "Solutions"
+            await test.step('Quand je clique sur le lien "Contactez-nous"', async () => {      //debut de l'etape 2
+                await page.click('text=Contactez-nous');      //cliquer sur le lien "Contactez-nous"
             });
+
+            await test.step('Alors je suis redirigé vers la page de contact', async () => {      //debut de l'etape 3
+                await page.goto('https://www.isagri.fr/contact');      //aller a la page de contact
         });      //fin du test scenario 1
     });      //fin du bloc de tests
-})();      //fin de la fonction asynchrone auto-executante
+    });
---
+++ Tests/05-navigation.spec.ts
@@ -0,0 +1,19 @@
+import { test, Browser, Page } from '@playwright/test';      //import des librairies necessaires
+
+
+test.describe('Navigation vers le site www.isagri.com', () => {      
+    test('Le lien principal redirige correctement', async({ page }) => {      
+        await test.step('Etant donné que je suis sur la page d\'accueil isagri', async () => {      
+            await page.goto('https://www.isagri.fr/');     
+        })
+
+        await test.step('Quand je clique sur le lien "Solutions"', async () => {      
+            await page.waitForURL('https://www.isagri.fr');    
+            await page.click('text=Solutions');  
+        })
+
+        await test.step('jai bien été redirigé vers la page des solutions', async () => {
+            await page.goto('https://www.isagri.fr/ressources/articles/solutions');
+        })
+    });      
+})
