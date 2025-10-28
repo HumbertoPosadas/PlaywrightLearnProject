f1b2afe61e0ba502e1107176a83aa3e530274d03
Humberto POSADAS <posadas.humberto@outlook.com>
2025-10-28 15:40:52 +0100

    FillText

---
+++ Tests/09-FillTextOnSandbox.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect} from '@playwright/test';
+
+/*
+Il est important de bin cibler les elements dont on veut remplir seulement sur des web elements de type input ou textarea.
+*/
+
+test.describe('Actions dans le Sandbox Automation', () => {
+    test('Remplir un champ de texte', async ({ page }) => {
+        await test.step('tant que je navigue vers le Sandbox automation', async () => {
+            await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/')
+        })
+
+        await test.step('Je remplie le champ Un aburrido texto', async () => {
+            //await page.getByRole('textbox', { name: 'Un aburrido texto' }).fill('Ceci est un texte rempli par Playwright');
+            await page.getByPlaceholder('Ingresá texto').fill('Ceci est un texte rempli par Playwright');
+        })
+        
+
+        
+    })
+    
+})
