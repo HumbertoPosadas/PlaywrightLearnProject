a8188e5f990196825a5f2612504d75dd6c87d2fd
Humberto POSADAS <posadas.humberto@outlook.com>
2025-11-04 15:42:03 +0100

    Soft assertions

---
+++ Tests/15-softAssertions.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+//Une soft assertion permet de continuer l'execution du test meme si une assertion echoue, 
+// ar exemple pour verifier plusieurs elements d'une page web si une assertion echoue, les autres assertions seront quand meme verifiees.
+
+test.describe('Soft assertions pour les checkboxes', () => {
+    test('Les assertions doivent continuer même si une échoue', async ({ page }) => {
+
+        await test.step('Je navigue vers le Sandbox automation', async () => {
+            await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/')
+        })
+        
+        await test.step('Je valide que tous les elements textboxes sont visibles', async () => {
+            await expect.soft(page.getByText('Burrito 🌯'), 'L\'élément Burrito 🌯 n\'est pas visible').toBeVisible();
+            await expect.soft(page.getByText('Pizza 🍕'), 'L\'élément Pizza 🍕 n\'est pas visible').toBeVisible();
+            await expect.soft(page.getByText('Pasta 🍝'), 'L\'élément Pasta 🍝 n\'est pas visible').toBeVisible();
+            await expect.soft(page.getByText('Sushi 🍣'), 'L\'élément Sushi 🍣 n\'est pas visible').toBeVisible();
+            await expect.soft(page.getByText('Helado 🍧'), 'L\'élément Helado 🍧 n\'est pas visible').toBeVisible();
+            await expect.soft(page.getByText('Torta 🍰'), 'L\'élément Torta 🍰 n\'est pas visible').toBeVisible();
+        })
+        
+    })
+    
+})
+
+
